98500ded544a02f283c08934e86a1c95ca551338
Alex <aliaksei.sholakhau@netcracker.com>
2021-12-30 15:51:01 +0300

    Added Contact page and updated Serivces page, added tabs

--- js/main.js
+++ js/main.js
@@ -19,4 +19,40 @@ const burgerFunction = () => {
 
 }
 
+function tabsFunction() {
+    const tabsControls = document.querySelectorAll('.tab-control');
+    const tabs = document.querySelectorAll('.service-tab');
+    tabsControls.forEach(control => {
+        control.addEventListener('click', e => {
+            resetTabs();
+            findTab(e.target.value);
+        })
+    });
+
+
+    function findTab(tabName) {
+        tabs.forEach(tab => {
+            if(tab.dataset.tabName === tabName) {
+                tab.classList.add('active');
+            }
+        });
+
+        tabsControls.forEach(control => {
+            if(control.value === tabName) {
+                control.classList.add('active');
+            }
+        })
+    }
+
+    function resetTabs() {
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        });
+        tabsControls.forEach(control => {
+            control.classList.remove('active');
+        })
+    }
+}
+
+tabsFunction();
 burgerFunction();
